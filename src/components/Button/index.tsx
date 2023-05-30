f3d8055c59c294ac2685f521d79e8ea2c40ebb68
João Vitral <joaoviniciusvitral@hotmail.com>
2023-05-30 16:25:04 -0300

    feat: adding new folders

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+const Button: React.FC = () => {
+  return <div>buttoa</div>
+}
+
+export default Button
---
+++ src/pages/teste/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// import { Container } from './styles';
+import Button from '@/components/Button';
+
+const teste: React.FC = () => {
+  return <Button />;
+}
+
+export default teste;
