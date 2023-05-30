d18fbd29165df0c624e5f3c5a5b1105bf78e5868
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-05-30 17:40:31 -0300

    feat: changing the navbar on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,15 +6,20 @@ import { Button } from "@/components";
 // import { Container } from './styles';
 
 const Header: React.FC = () => {
+  const navitems = [
+    "Início",
+    "Cenário dos Precatórios",
+    "Sobre Nós",
+    "Contato",
+    "Notícias",
+  ];
   return (
     <C.Container>
       <C.Logo src="https://images.unsplash.com/photo-1685371863623-effd71822cf2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1000&q=60" />
       <C.Navbar>
-        <C.NavbarItem>Início</C.NavbarItem>
-        <C.NavbarItem>Cenário dos Precatórios</C.NavbarItem>
-        <C.NavbarItem>Sobre Nós</C.NavbarItem>
-        <C.NavbarItem>Contato</C.NavbarItem>
-        <C.NavbarItem>Notícias</C.NavbarItem>
+        {navitems.map((item, index) => {
+          return <C.NavbarItem key={index}>{item}</C.NavbarItem>;
+        })}
       </C.Navbar>
       <Button text="Fale conosco" />
     </C.Container>
