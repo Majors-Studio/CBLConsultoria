b2ec7738312291c0cc96af832d993ef341a2cbc6
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-05-30 16:45:34 -0300

    feat: creating header and footer

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // import { Container } from './styles';
 
 const about: React.FC = () => {
-  return <div>Olá</div>;
+  return <div>Olá mundo!</div>;
 };
 
 export default about;
