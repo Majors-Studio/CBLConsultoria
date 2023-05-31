8c6d1cdaa321f7e55a67dcd0e7c7d8cdb88a9525
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-05-31 01:23:05 -0300

    Update Subtitle.tsx

--- src/components/Subtitle/index.tsx
+++ src/components/Subtitle/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import * as C from "./styles";
-// import { Container } from './styles';
+import { iProps } from "./styles";
 
-const Subtitle: React.FC = () => {
-  return <div />;
+interface SubtitleProps extends iProps {
+  text?: string;
+  fontWeight?: string;
+}
+
+const Subtitle: React.FC<SubtitleProps> = ({ text, fontSize, fontWeight }) => {
+  return (
+    <C.Subtitle
+      style={{
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      }}
+    >
+      {text}
+    </C.Subtitle>
+  );
 };
 
 export default Subtitle;
