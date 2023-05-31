0fc1ec4471f51814cdde283dc18a54d0daaef378
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-05-31 01:23:51 -0300

    Update styles.ts

--- src/pages/about/styles.ts
+++ src/pages/about/styles.ts
@@ -1,10 +1,31 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
-export const ContainerImg = styled.div``;
-export const FlexCol = styled.div``;
+export const ContainerImg = styled.div`
+  width: 100%;
+  max-width: 100vw;
+  position: relative;
+  margin-bottom: 71px;
+`;
+export const FlexCol = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export const MeetTitle = styled.h1``;
 export const Video = styled.video``;
-export const JustifyBetween = styled.div``;
+export const JustifyBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 export const Title = styled.h1``;
 export const Description = styled.p``;
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-width: 100vw;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
