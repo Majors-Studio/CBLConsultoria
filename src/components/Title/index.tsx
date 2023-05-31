4aa74f30f39e3c82d4ff97eebfe3226430a6444a
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-05-31 01:22:21 -0300

    Update Title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import * as C from "./styles";
+import { iTitle } from "./styles";
+interface TitleProps extends iTitle {
+  text?: string;
+  fontWeight?: string;
+  color?: string;
+  position?: string | any;
+  top?: string;
+  left?: string;
+  transform?: string;
+}
 
-// import { Container } from './styles';
-
-const Title: React.FC = () => {
-  return <C.Title></C.Title>;
+const Title: React.FC<TitleProps> = ({
+  text,
+  fontSize,
+  fontWeight,
+  color,
+  position,
+  top,
+  left,
+  transform,
+}) => {
+  return (
+    <C.Title
+      style={{
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+        position: position,
+        top: top,
+        left: left,
+        transform: transform,
+      }}
+    >
+      {text}
+    </C.Title>
+  );
 };
 
 export default Title;
