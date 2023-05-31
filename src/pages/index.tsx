8448d3d24845125f439f46ea8e97c7ae448d449f
João Vitral <joaoviniciusvitral@hotmail.com>
2023-05-31 00:05:38 -0300

    fix: styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ export default function Home() {
     <div>
       <Head>
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:image" content="/images/og/harmony.png" />
-        <link rel="manifest" href="site.webmanifest" />
+        {/* <meta name="robots" content="index, follow" /> */}
+        {/* <meta property="og:image" content="/images/og/harmony.png" /> */}
+        {/* <link rel="manifest" href="site.webmanifest" /> */}
       </Head>
       <h1>Home</h1>
     </div>
