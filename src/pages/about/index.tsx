bebb60936e0436756fe3a33ebf7ca65a1fcbe56b
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-05-31 01:23:38 -0300

    Update index.tsx

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import * as C from "./styles";
+import Image from "next/image";
+import { Subtitle, Title } from "@/components";
+
+import bgImg from "../../../public/assets/images/bgAbout.jpg";
 // import { Container } from './styles';
 
 const about: React.FC = () => {
   return (
     <C.Container>
-      <C.ContainerImg></C.ContainerImg>
+      <C.ContainerImg>
+        <Image
+          style={{
+            position: "relative",
+            height: "200px",
+            objectFit: "cover",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+          src={bgImg}
+          alt="bg"
+        />
+        <C.Overlay />
+        <Title
+          color="#fff"
+          top="50%"
+          fontWeight="bold"
+          fontSize="40px"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          position="absolute"
+          text="Sobre nós"
+        />
+      </C.ContainerImg>
 
       <C.FlexCol>
-        <C.MeetTitle></C.MeetTitle>
+        <Title fontSize="36px" fontWeight="bold" text="Conheça a D.Andrade" />
         <C.Video />
       </C.FlexCol>
 
@@ -16,12 +44,16 @@ const about: React.FC = () => {
         {/* Componente com image, title e text */}
       </C.JustifyBetween>
 
-      <C.Title>Somos líderes em antecipações de precatório no Brasil</C.Title>
-      <C.Description></C.Description>
+      <Title
+        fontSize="29px"
+        fontWeight="600"
+        text="Somos líderes em antecipações de precatórios no Brasil"
+      />
+      <Subtitle text="A D.Andrade  é referência em precatórios. Com mais de uma década de experiência negociamos mais de R$ 1 bilhão em precatórios e contramos com a satisfação de mais de 7 mil clientes." />
 
       {/* Componente com ( subtititle e text) */}
 
-      <C.Title>Faça o tour virtual pelo nosso escritório</C.Title>
+      <Title text="Nossa missão" />
       <C.Video></C.Video>
 
       <C.Title></C.Title>
