c6f3e275476693cd6f13e5276669578588595edf
João Vitral <joaoviniciusvitral@hotmail.com>
2023-05-30 23:56:17 -0300

    fix: adding Head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,6 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
-        {/* <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" /> */}
-        {/* <meta name="robots" content="index, follow" /> */}
-        {/* <meta property="og:image" content="/images/og/harmony.png" /> */}
-        {/* <link rel="manifest" href="site.webmanifest" /> */}
       </Head>
       <Layout>
         <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
+import Head from "next/head"
+
 export default function Home() {
   return (
     <div>
+      <Head>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:image" content="/images/og/harmony.png" />
+        <link rel="manifest" href="site.webmanifest" />
+      </Head>
       <h1>Home</h1>
     </div>
   )
