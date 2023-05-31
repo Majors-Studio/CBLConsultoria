6bacedbbef4d5d28520a8809de2b594b3a8b637a
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-05-31 01:23:18 -0300

    Update styles.ts

--- src/components/Subtitle/styles.ts
+++ src/components/Subtitle/styles.ts
@@ -1,1 +1,11 @@
 import styled from "styled-components";
+
+export interface iProps {
+  fontSize?: string;
+}
+
+export const Subtitle = styled.p<iProps>`
+  font-size: ${(props) => props.fontSize || "22px"};
+  color: #823b1d;
+  text-align: center;
+`;
