77945fb16755897438bf52ed1b0c8ad74435f357
João Vitral <joaoviniciusvitral@hotmail.com>
2023-05-30 23:43:42 -0300

    fix: errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,10 @@
-import Document, { Html, Main, NextScript } from "next/document"
+import Document, { Head, Html, Main, NextScript } from "next/document"
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html>
+        <Head/>
         <body>
           <Main />
           <NextScript />
