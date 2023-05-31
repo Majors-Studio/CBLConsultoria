51b7150a338d8411b0f16d177cfae8338f85671d
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-05-30 23:56:34 -0300

    feat: creating Title and Subtitle components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Button } from "./Button";
+export { default as Title } from "./Title";
+export { default as Subtitle } from "./Subtitle";
