df36a1030571455f0ad4d6df409827ec1a2a0d97
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-06-06 21:54:20 -0300

    feat: adding navitems on utils folder

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,61 +1,60 @@
-"use client"
+"use client";
 
-import React, { useState } from "react"
-import * as C from "./styles"
-import { Button } from "@/components"
-import Logo from "../Logo"
-import { useDevice } from "@/hooks/useDevice"
+import React, { useState } from "react";
+import * as C from "./styles";
+import { Button } from "@/components";
+import Logo from "../Logo";
+import { useDevice } from "@/hooks/useDevice";
+
+import { navitems } from "@/utils/navitems";
+import Link from "next/link";
 
 const Header: React.FC = () => {
-  const { isMobile } = useDevice()
-  const [isOpenAccordion, setIsOpenAccordion] = useState(false)
+  const { isMobile } = useDevice();
+  const [isOpenAccordion, setIsOpenAccordion] = useState(false);
 
   const handleOpenAccordion = () => {
-    setIsOpenAccordion(!isOpenAccordion)
-  }
+    setIsOpenAccordion(!isOpenAccordion);
+  };
 
-  const navitems = [
-    "Início",
-    "Cenário dos Precatórios",
-    "Sobre Nós",
-    "Contato",
-    "Notícias",
-  ]
   return (
     <C.Container>
       <C.Content>
-      <Logo />
-      <div>
-        {isMobile && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-8 h-8 relative cursor-pointer"
-            onClick={handleOpenAccordion}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        )}
+        <Logo />
+        <div>
+          {isMobile && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-8 h-8 relative cursor-pointer"
+              onClick={handleOpenAccordion}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          )}
 
-        <C.Navbar>
-          {navitems.map((item, index) => {
-            return <C.NavbarItem key={index}>{item}</C.NavbarItem>
-          })}
-        </C.Navbar>
-      </div>
+          <C.Navbar>
+            {navitems.map((item, index) => {
+              return (
+                <C.NavbarItem key={index}>
+                  <Link href={item.url}>{item.title}</Link>
+                </C.NavbarItem>
+              );
+            })}
+          </C.Navbar>
+        </div>
 
         <Button text="Fale Conosco" />
       </C.Content>
-        
     </C.Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/utils/navitems.ts
@@ -0,0 +1,27 @@
+export const navitems = [
+  {
+    id: 1,
+    title: "Início",
+    url: "",
+  },
+  {
+    id: 2,
+    title: "Cenário dos Precatórios",
+    url: "",
+  },
+  {
+    id: 3,
+    title: "Sobre Nós",
+    url: "/about",
+  },
+  {
+    id: 4,
+    title: "Contato",
+    url: "",
+  },
+  {
+    id: 5,
+    title: "Notícias",
+    url: "",
+  },
+];
