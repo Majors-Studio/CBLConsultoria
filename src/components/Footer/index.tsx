c22520464d44eefd20b244aa4033387c9be100fc
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-06-14 00:07:27 -0300

    feat: editing footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,36 @@
-import React from "react"
+import React from "react";
 
-import * as S from "./styles"
-import Logo from "../Logo"
+// google maps api
+import { useLoadScript, GoogleMap, MarkerF } from "@react-google-maps/api";
+import { useMemo } from "react";
+import * as S from "./styles";
+import Logo from "../Logo";
 
-import Image from 'next/image'
+import Image from "next/image";
 
 const Footer: React.FC = () => {
+  // google maps config
+  const libraries = useMemo(() => ["places"], []);
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    libraries: libraries as any,
+  });
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+
+  // map position
+  const mapCenter = useMemo(
+    () => ({ lat: 27.672932021393862, lng: 85.31184012689732 }),
+    []
+  );
+
   const midiaItems = [
     {
       name: "Linkedin",
@@ -22,7 +47,18 @@ const Footer: React.FC = () => {
       url: "https://www.instagram.com/",
       logo: "https://static.wixstatic.com/media/fea1c1_02a47dd2d3a14f7a908448826a993500~mv2.png/v1/fill/w_36,h_36,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/%C3%8Dcone%20Instagram.png",
     },
-  ]
+  ];
+
+  const middleInfoData = [
+    "Política de privacidade",
+    "Termos de uso",
+    "Contato",
+    "Sobre",
+  ];
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <S.Container>
@@ -32,14 +68,71 @@ const Footer: React.FC = () => {
           {midiaItems.map((item, index) => (
             <S.MidiaItem key={index} href={item.url}>
               <Image width={36} height={36} src={item.logo} alt={item.name} />
+              <S.InfoText>{item.name}</S.InfoText>
             </S.MidiaItem>
           ))}
         </S.MidiaItems>
       </S.Midia>
-      <S.Info></S.Info>
-      <S.Nav></S.Nav>
-    </S.Container>
-  )
-}
+      <S.MiddleInfo>
+        {middleInfoData.map((item, index) => (
+          <S.Subtitle>{item}</S.Subtitle>
+        ))}
+      </S.MiddleInfo>
+      <S.Info>
+        <S.Title>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"
+            />
+          </svg>
+          Localização
+        </S.Title>
+        <GoogleMap
+          options={mapOptions}
+          zoom={14}
+          center={mapCenter}
+          mapTypeId={google.maps.MapTypeId.ROADMAP}
+          mapContainerStyle={{ width: "500px", height: "200px" }}
+          onLoad={() => console.log("Map Component Loaded...")}
+        >
+          <MarkerF
+            position={mapCenter}
+            icon="https://picsum.photos/64"
+            onLoad={() => console.log("Marker Loaded")}
+          />
+        </GoogleMap>
+        <S.Subtitle>Rua Timote Silva, 280</S.Subtitle>
 
-export default Footer
+        <S.Title>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
+            />
+          </svg>
+          Email
+        </S.Title>
+        <S.Subtitle>Clbconsultoria@gmail.com</S.Subtitle>
+      </S.Info>
+    </S.Container>
+  );
+};
+
+export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,28 +1,80 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.footer`
   background-color: #040506;
+  max-width: 100vw;
   width: 100%;
-  height: 200px;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 46px 60px;
 
-  padding: 46px 20px;
-`
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 24px;
+    padding: 24px 16px;
+  }
+`;
 
 export const Midia = styled.div`
-display: flex;
-flex-direction: column;
-gap: 12px;
-
-`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+`;
 
 export const MidiaItems = styled.div`
   display: flex;
   align-items: center;
   gap: 12px;
-`
+`;
 
-export const MidiaItem = styled.a``
+export const MidiaItem = styled.a`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const Info = styled.div``
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
 
-export const Nav = styled.div``
+export const Title = styled.h3`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 32px;
+`;
+
+export const Subtitle = styled.h4`
+  color: #fff;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+export const Nav = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const InfoText = styled.p`
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 24px;
+`;
+
+export const MiddleInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+`;
