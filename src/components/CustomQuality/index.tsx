dc41255a637c3c2bd51cecd17ca9150033a81819
João Vitral <joaoviniciusvitral@hotmail.com>
2023-06-30 00:48:22 -0300

    feat: finishing about refact

--- src/components/CustomQuality/index.tsx
+++ src/components/CustomQuality/index.tsx
@@ -1,87 +1,88 @@
-import React from "react";
-import * as C from "./styles";
+import React from "react"
+import * as C from "./styles"
 
-import Image from "next/image";
-import { Title, Subtitle } from "@/components";
+import Image from "next/image"
+import { Title, Subtitle } from "@/components"
 
 // Swiper configs
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Autoplay, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination } from "swiper"
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
 
-import { useDevice } from "@/hooks/useDevice";
+import { useDevice } from "@/hooks/useDevice"
+import ContentBox from "../ContentBox"
+import { tokens } from "@/utils/tokens"
+import { useAbout } from "@/context/aboutContext"
 
-interface CustomQualityProps {
-  data: {
-    id: number;
-    img: string;
-    title: string;
-    subtitle: string;
-  }[];
+
+const CustomQuality: React.FC = ( ) => {
+  const { isMobile } = useDevice()
+  const { qualityList } = useAbout()
+  
+
+  return (
+    <ContentBox bgColor={tokens.colors.brand.lightCream} py={"60px"}>
+      {isMobile ? (
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          modules={[Pagination]}
+          navigation={false}
+          autoplay={{ delay: 3000 }}
+          pagination={{ clickable: true }}
+        >
+          {qualityList.map((item, index) => (
+            <SwiperSlide key={index}>
+              <C.Item key={item.id}>
+                <Image
+                  style={{
+                    width: "75px",
+                    height: "75px",
+                  }}
+                  src={item.img}
+                  width={75}
+                  height={75}
+                  alt="img"
+                />
+                <Title>{item.title}</Title>
+                <Subtitle>{item.subtitle}</Subtitle>
+              </C.Item>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <C.Container>
+          <C.Content>
+            {qualityList.map((item, index) => (
+              <C.Item key={index}>
+                <Image
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                  }}
+                  src={item.img}
+                  width={50}
+                  height={50}
+                  alt="img"
+                />
+                <Title
+                  style={{
+                    fontSize: "1.5rem",
+                  }}
+                >
+                  {item.title}
+                </Title>
+                <Subtitle>{item.subtitle}</Subtitle>
+              </C.Item>
+            ))}
+          </C.Content>
+        </C.Container>
+      )}
+    </ContentBox>
+  )
 }
 
-const CustomQuality: React.FC<CustomQualityProps> = ({ data }) => {
-  const { isMobile } = useDevice();
-
-  return isMobile ? (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      modules={[Pagination]}
-      navigation={false}
-      autoplay={{ delay: 3000 }}
-      pagination={{ clickable: true }}
-    >
-      {data.map((item, index) => (
-        <SwiperSlide key={index}>
-          <C.Item key={item.id}>
-            <Image
-              style={{
-                width: "75px",
-                height: "75px",
-              }}
-              src={item.img}
-              width={75}
-              height={75}
-              alt="img"
-            />
-            <Title>{item.title}</Title>
-            <Subtitle>{item.subtitle}</Subtitle>
-          </C.Item>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  ) : (
-    <C.Container>
-      <C.Content>
-        {data.map((item, index) => (
-          <C.Item key={index}>
-            <Image
-              style={{
-                width: "50px",
-                height: "50px",
-              }}
-              src={item.img}
-              width={50}
-              height={50}
-              alt="img"
-            />
-            <Title
-              style={{
-                fontSize: "1.5rem",
-              }}
-            >
-              {item.title}
-            </Title>
-            <Subtitle>{item.subtitle}</Subtitle>
-          </C.Item>
-        ))}
-      </C.Content>
-    </C.Container>
-  );
-};
-
-export default CustomQuality;
+export default CustomQuality
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,24 +1,17 @@
 import React from "react"
-import * as C from "@/styles/about"
 import { CustomQuality } from "@/components"
-import { useAbout } from "@/context/aboutContext"
 import HeadBanner from "@/components/HeadBanner"
 import Purpose from "@/sections/Purpose"
-
 import AvatarList from "@/sections/AvatarList"
 import SlabList from "@/sections/SlabList"
 import VideoHistory from "@/sections/VideoHistory"
 
 const About: React.FC = () => {
-  const { qualityList } = useAbout()
-
   return (
-    <C.Container>
+    <>
       <HeadBanner title="Sobre Nós" />
 
-      <C.SwiperContainer>
-        <CustomQuality data={qualityList} />
-      </C.SwiperContainer>
+      <CustomQuality />
 
       <SlabList />
 
@@ -27,7 +20,7 @@ const About: React.FC = () => {
       <AvatarList />
 
       <Purpose />
-    </C.Container>
+    </>
   )
 }
 
