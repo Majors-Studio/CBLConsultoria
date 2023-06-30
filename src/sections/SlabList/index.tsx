6c515c60431f0f1055ef05f1197b0315cc1498c8
João Vitral <joaoviniciusvitral@hotmail.com>
2023-06-30 00:49:14 -0300

    fix: slablist bgColor

--- src/sections/SlabList/index.tsx
+++ src/sections/SlabList/index.tsx
@@ -1,13 +1,14 @@
 import { Card } from "@/components"
 import ContentBox from "@/components/ContentBox"
 import { useAbout } from "@/context/aboutContext"
+import { tokens } from "@/utils/tokens"
 import React from "react"
 
 const SlabList: React.FC = () => {
   const { cardsList } = useAbout()
 
   return (
-    <ContentBox py={"60px"}>
+    <ContentBox py={"60px"} bgColor={tokens.colors.brand.light}>
       {cardsList.map((card, index) => (
         <Card
           key={index}
