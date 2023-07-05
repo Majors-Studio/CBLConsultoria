20febe56957a198574b6d969d8b8ddaf0399cf45
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-07-05 16:43:47 -0300

    feat: adding toast

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
+import { Toast } from "@/components";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Button } from "./Button";
 export { default as Title } from "./Title";
 export { default as Subtitle } from "./Subtitle";
 export { default as CustomQuality } from "./CustomQuality";
+export { default as Toast } from "./Toast";
 export { default as Card } from "./Card";
