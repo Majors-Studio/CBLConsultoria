9ca646194332a3eff9a7c26eb85e0acedf9c8d3e
João Vitral <joaoviniciusvitral@hotmail.com>
2023-07-05 00:42:45 -0300

    fix

--- src/sections/SlabList/index.tsx
+++ src/sections/SlabList/index.tsx
@@ -1,12 +1,10 @@
 import { Card } from "@/components"
 import ContentBox from "@/components/ContentBox"
-import { useAbout } from "@/context/aboutContext"
 import { cardsList } from "@/utils/dataObjects"
 import { tokens } from "@/utils/tokens"
 import React from "react"
 
 const SlabList: React.FC = () => {
-
   return (
     <ContentBox py={"60px"} bgColor={tokens.colors.brand.light}>
       {cardsList.map((card, index) => (
