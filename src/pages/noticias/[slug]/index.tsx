82f851194522ab3b529e47de53f5ea967586aa40
João Vitral <joaoviniciusvitral@hotmail.com>
2023-07-16 22:10:27 -0300

    feat: styling news slug

--- src/pages/noticias/[slug]/index.tsx
+++ src/pages/noticias/[slug]/index.tsx
@@ -21,7 +21,6 @@ const Page: React.FC<any> = () => {
   } = router
 
   useEffect(() => {
-    if (newsList.length > 0) return
     getNewsList()
   }, [])
 
@@ -53,20 +52,20 @@ const Page: React.FC<any> = () => {
       [MARKS.CODE]: (text) => <code>{text}</code>,
     },
     renderNode: {
-      [BLOCKS.PARAGRAPH]: (node, children) => <p>{children}</p>,
-      [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
-      [BLOCKS.HEADING_2]: (node, children) => <h2>{children}</h2>,
-      [BLOCKS.HEADING_3]: (node, children) => <h3>{children}</h3>,
-      [BLOCKS.HEADING_4]: (node, children) => <h4>{children}</h4>,
-      [BLOCKS.HEADING_5]: (node, children) => <h5>{children}</h5>,
-      [BLOCKS.HEADING_6]: (node, children) => <h6>{children}</h6>,
-      [BLOCKS.UL_LIST]: (node, children) => <ul>{children}</ul>,
-      [BLOCKS.OL_LIST]: (node, children) => <ol>{children}</ol>,
-      [BLOCKS.LIST_ITEM]: (node, children) => <li>{children}</li>,
-      [BLOCKS.QUOTE]: (node, children) => <blockquote>{children}</blockquote>,
+      [BLOCKS.PARAGRAPH]: (node, children) => <S.Paragraph>{children}</S.Paragraph>,
+      [BLOCKS.HEADING_1]: (node, children) => <S.Heading1>{children}</S.Heading1>,
+      [BLOCKS.HEADING_2]: (node, children) => <S.Heading2>{children}</S.Heading2>,
+      [BLOCKS.HEADING_3]: (node, children) => <S.Heading3>{children}</S.Heading3>,
+      [BLOCKS.HEADING_4]: (node, children) => <S.Heading4>{children}</S.Heading4>,
+      [BLOCKS.HEADING_5]: (node, children) => <S.Heading5>{children}</S.Heading5>,
+      [BLOCKS.HEADING_6]: (node, children) => <S.Heading6>{children}</S.Heading6>,
+      [BLOCKS.UL_LIST]: (node, children) => <S.UlList>{children}</S.UlList>,
+      [BLOCKS.OL_LIST]: (node, children) => <S.OlList>{children}</S.OlList>,
+      [BLOCKS.LIST_ITEM]: (node, children) => <S.ListItem>{children}</S.ListItem>,
+      [BLOCKS.QUOTE]: (node, children) => <S.Quote>{children}</S.Quote>,
       [BLOCKS.HR]: (node, children) => <hr />,
       [BLOCKS.EMBEDDED_ASSET]: (node, children) => (
-        <img src={node.data.target.fields.file.url} />
+        <S.Image src={node.data.target.fields.file.url} />
       ),
       [BLOCKS.EMBEDDED_ENTRY]: (node, children) => <div>{children}</div>,
     },
@@ -90,7 +89,7 @@ const Page: React.FC<any> = () => {
         }}
       >
         {featuredImage ? (
-          <S.Image
+          <S.BannerImage
             src={featuredImage?.fields.file.url}
             alt={featuredImage.fields.title?.toString()}
           />
@@ -104,11 +103,7 @@ const Page: React.FC<any> = () => {
           {" | "}
           <span>{getDate(publishedDate?.toString() || "")}</span>
         </S.Description>
-        <S.ContentText>
-          <div>
-          {documentToReactComponents(content, options)}
-          </div>
-        </S.ContentText>
+        {documentToReactComponents(content, options)}
       </S.Content>
     </S.Container>
   )
--- src/styles/news_slug.ts
+++ src/styles/news_slug.ts
@@ -61,7 +61,7 @@ export const Right = styled.div`
   flex-direction: column;
 `
 
-export const Image = styled.img`
+export const BannerImage = styled.img`
   width: 100%;
 
   max-height: 400px;
@@ -119,9 +119,61 @@ export const Description = styled.div`
   }
 `
 
-export const ContentText = styled.div``
+export const Heading1 = styled.h1`
+  font-size: 28px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 40px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.primary};
+`
 
-export const Text = styled.p`
+export const Heading2 = styled.h2`
+  font-size: 24px;
+  color: ${tokens.colors.neutral.lowDark};  
+  font-weight: 400;
+  line-height: 35px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.primary};
+` 
+
+export const Heading3 = styled.h3`
+  font-size: 20px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.primary};
+`
+
+export const Heading4 = styled.h4`
+  font-size: 18px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.primary};
+`
+
+export const Heading5 = styled.h5`
+  font-size: 16px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.primary};
+`
+
+export const Heading6 = styled.h6`
+  font-size: 14px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.primary};
+`
+
+export const Paragraph = styled.p`
   font-size: 18px;
   color: ${tokens.colors.neutral.lowDark};
   font-weight: 400;
@@ -129,3 +181,60 @@ export const Text = styled.p`
   margin-bottom: 12px;
   font-family: ${tokens.font.family.secondary};
 `
+
+export const Quote = styled.blockquote`
+  font-size: 18px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.secondary};
+`
+
+export const Code = styled.code`
+  font-size: 18px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.secondary};
+`
+
+export const UlList = styled.ul`
+  font-size: 18px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.secondary};
+`
+
+export const OlList = styled.ol`
+  font-size: 18px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 30px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.secondary};
+`
+
+export const ListItem = styled.li`
+  font-size: 18px;
+  color: ${tokens.colors.neutral.lowDark};
+  font-weight: 400;
+  line-height: 25px;
+  margin-bottom: 12px;
+  font-family: ${tokens.font.family.secondary};
+`
+
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  object-position: center;
+
+  margin-bottom: 12px;
+`
+
+
+
