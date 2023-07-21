ef0aa6725c3abcf0a6b19b080ffb9aec3069549a
João Vitral <joaoviniciusvitral@hotmail.com>
2023-07-21 16:38:04 -0300

    fix: styles updates

--- src/pages/consultoria-juridica/index.tsx
+++ src/pages/consultoria-juridica/index.tsx
@@ -1,136 +1,175 @@
-import React from "react";
-import { ConfirmIcon } from "@/assets/icons";
+import React from "react"
+import { ConfirmIcon } from "@/assets/icons"
 
-import * as C from "@/styles/legalAdvice";
-import Image from "next/image";
-import PurposeForm from "@/components/PurposeForm";
-import HeadBanner from "@/components/HeadBanner";
-import img2 from "../../assets/png/chaves.png";
-import img3 from "../../assets/png/medic.png";
-import img4 from "../../assets/png/reunion.png";
-import img5 from "../../assets/png/consult.jpg";
-import { tokens } from "@/utils/tokens";
-import { Subtitle, Title } from "@/components";
-import ContentBox from "@/components/ContentBox";
-import { civelList } from "@/utils/dataObjects";
-import Anchor from "@/components/Anchor";
+import * as C from "@/styles/legalAdvice"
+import Image from "next/image"
+import PurposeForm from "@/components/PurposeForm"
+import HeadBanner from "@/components/HeadBanner"
+import img2 from "../../assets/png/chaves.png"
+import img3 from "../../assets/png/medic.png"
+import img4 from "../../assets/png/reunion.png"
+import img5 from "../../assets/png/consult.jpg"
+import { tokens } from "@/utils/tokens"
+import { Subtitle, Title } from "@/components"
+import ContentBox from "@/components/ContentBox"
+import { civelList } from "@/utils/dataObjects"
+import Anchor from "@/components/Anchor"
+import { useDevice } from "@/hooks/useDevice"
 
 const LegalAdvice: React.FC = () => {
+  const { isMobile } = useDevice()
   return (
     <>
-      <HeadBanner title="Consultoria juridica" color={tokens.colors.brand.light} />
+      <HeadBanner
+        title="Consultoria juridica"
+        color={tokens.colors.brand.light}
+      />
 
-      <C.ContainerFluid bgColor={`${tokens.colors.brand.cta}`}>
-        <C.Container>
-          <C.BoxText>
-            <Anchor id="real-estate" />
-            <Title style={{ fontWeight: "bold" }} variant="secondary">
-              REGULARIZAÇÃO DE IMÓVEIS
-            </Title>
-            <C.Text textColor={`${tokens.colors.brand.light}`}>
-              Regularizamos qualquer problema com seu imóvel. Você sabia que a
-              regularização do seu imóvel, urbano ou rural, é capaz de
-              valorizá-lo em até 50%? Além disso, traz uma série de benefícios
-              na alienação e sucessão desses bens.
-              <br />
-              <br />
-              Os donos de imóveis rurais são impedidos de participar de
-              privilégios na obtenção de créditos. Resguarde o seu patrimônio e
-              regularize agora o seu imóvel
-            </C.Text>
-          </C.BoxText>
-          <C.BoxImage>
-            <Image src={img2} alt="regularizacao" priority />
-          </C.BoxImage>
-        </C.Container>
-      </C.ContainerFluid>
+      <ContentBox
+        py={"60px"}
+        bgColor={`${tokens.colors.brand.cta}`}
+        contentStyle={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: isMobile ? "column-reverse" : "row",
+          gap: isMobile ? "24px" : undefined,
+        }}
+      >
+        <C.BoxText>
+          <Anchor id="real-estate" />
+          <Title
+            style={{ fontWeight: "bold", textAlign: "left" }}
+            variant="secondary"
+          >
+            REGULARIZAÇÃO DE IMÓVEIS
+          </Title>
+          <Subtitle
+            style={{ textAlign: "left", color: tokens.colors.brand.light }}
+          >
+            Regularizamos qualquer problema com seu imóvel. Você sabia que a
+            regularização do seu imóvel, urbano ou rural, é capaz de valorizá-lo
+            em até 50%? Além disso, traz uma série de benefícios na alienação e
+            sucessão desses bens.
+            <br />
+            <br />
+            Os donos de imóveis rurais são impedidos de participar de
+            privilégios na obtenção de créditos. Resguarde o seu patrimônio e
+            regularize agora o seu imóvel
+          </Subtitle>
+        </C.BoxText>
 
-      <C.ContainerFluid bgColor={`${tokens.colors.brand.lightCream}`}>
-        <C.Container>
-          <C.BoxImage>
-            <Image src={img3} alt="regularizacao" priority />
-          </C.BoxImage>
-          <C.BoxText>
-            <Anchor id="medical-error" />
-            <Title style={{ fontWeight: "bold", textAlign: "right" }}>
-              Erro médico
-            </Title>
-            <Subtitle style={{ textAlign: "right" }}>
-              A Organização Mundial da Saúde (OMS) publicou um relatório que
-              mostra um aumento na ocorrência de erros médicos em todo o mundo e
-              no Brasil não é diferente.
-              <br />
-              <br />O acesso à justiça nesta área deve ser promovida por
-              profissionais capacitados e com experiência na atividade, seja em
-              âmbito judicial ou perante os Conselhos Regionais e Federal de
-              Medicina (CRM e CFM).
-            </Subtitle>
-          </C.BoxText>
-        </C.Container>
-      </C.ContainerFluid>
-
-      <ContentBox bgColor={`${tokens.colors.brand.cta}`}>
-        <C.Container>
-          <C.BoxText>
-            <Title
-              variant="secondary"
-              style={{ fontWeight: "bold", textAlign: "left" }}
-            >
-              Erro médico
-            </Title>
-            <Subtitle style={{ color: tokens.colors.brand.light, textAlign: "left" }}>
-              A Organização Mundial da Saúde (OMS) publicou um relatório que
-              mostra um aumento na ocorrência de erros médicos em todo o mundo e
-              no Brasil não é diferente.
-              <br />
-              <br />O acesso à justiça nesta área deve ser promovida por
-              profissionais capacitados e com experiência na atividade, seja em
-              âmbito judicial ou perante os Conselhos Regionais e Federal de
-              Medicina (CRM e CFM).
-            </Subtitle>
-          </C.BoxText>
-          <C.BoxImage>
-            <Image src={img4} alt="regularizacao" priority />
-          </C.BoxImage>
-        </C.Container>
+        <C.BoxImage>
+          <Image src={img2} alt="regularizacao" priority />
+        </C.BoxImage>
       </ContentBox>
-      <C.ContainerFluid bgColor={`${tokens.colors.brand.lightCream}`}>
-        <C.Container>
-          <C.BoxImage>
-            <Image src={img5} alt="regularizacao" priority />
-          </C.BoxImage>
-          <C.BoxText>
-            <Anchor id="civil-causes" />
-            <Title
-              variant="secondary"
-              style={{ textAlign: "left", color: "#000" }}
-            >
-              Por que contratar uma consultoria Jurídica?
-            </Title>
-            <Subtitle style={{ color: "#000", textAlign: "left" }}>
-              Em casos cíveis, é de extrema importância buscar o aconselhamento
-              jurídico especializado para alcançar uma resolução justa e
-              eficiente. Aqui na CBL Consultoria, compreendemos a complexidade
-              do sistema legal e o impacto significativo que uma representação
-              adqueada pode ter em seu caso. Veja por que é fundamental contar
-              com um advogado especializado
-              <div style={{ marginTop: "20px" }}>
-                {civelList.map((item) => (
-                  <C.CivelListInfoItem key={item.id}>
-                    <ConfirmIcon />
-                    <Subtitle style={{ color: "#000" }}>{item.title}</Subtitle>
-                  </C.CivelListInfoItem>
-                ))}
-              </div>
-            </Subtitle>
-          </C.BoxText>
-        </C.Container>
-        {/* <PurposeForm /> */}
-      </C.ContainerFluid>
-      {/* <C.Bg> */}
+
+      <ContentBox
+        py={"60px"}
+        bgColor={`${tokens.colors.brand.lightCream}`}
+        contentStyle={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: isMobile ? "column-reverse" : "row",
+          gap: isMobile ? "24px" : undefined,
+        }}
+      >
+        <C.BoxImage>
+          <Image src={img3} alt="regularizacao" priority />
+        </C.BoxImage>
+        <C.BoxText>
+          <Anchor id="medical-error" />
+          <Title style={{ fontWeight: "bold", textAlign: "right" }}>
+            Erro médico
+          </Title>
+          <Subtitle style={{ textAlign: "right" }}>
+            A Organização Mundial da Saúde (OMS) publicou um relatório que
+            mostra um aumento na ocorrência de erros médicos em todo o mundo e
+            no Brasil não é diferente.
+            <br />
+            <br />O acesso à justiça nesta área deve ser promovida por
+            profissionais capacitados e com experiência na atividade, seja em
+            âmbito judicial ou perante os Conselhos Regionais e Federal de
+            Medicina (CRM e CFM).
+          </Subtitle>
+        </C.BoxText>
+      </ContentBox>
+
+      <ContentBox
+        py={"60px"}
+        bgColor={`${tokens.colors.brand.cta}`}
+        contentStyle={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: isMobile ? "column-reverse" : "row",
+          gap: isMobile ? "24px" : undefined,
+        }}
+      >
+        <C.BoxText>
+          <Title
+            variant="secondary"
+            style={{ fontWeight: "bold", textAlign: "left" }}
+          >
+            Erro médico
+          </Title>
+          <Subtitle
+            style={{ color: tokens.colors.brand.light, textAlign: "left" }}
+          >
+            A Organização Mundial da Saúde (OMS) publicou um relatório que
+            mostra um aumento na ocorrência de erros médicos em todo o mundo e
+            no Brasil não é diferente.
+            <br />
+            <br />O acesso à justiça nesta área deve ser promovida por
+            profissionais capacitados e com experiência na atividade, seja em
+            âmbito judicial ou perante os Conselhos Regionais e Federal de
+            Medicina (CRM e CFM).
+          </Subtitle>
+        </C.BoxText>
+        <C.BoxImage>
+          <Image src={img4} alt="regularizacao" priority />
+        </C.BoxImage>
+      </ContentBox>
+
+      <ContentBox
+        py={"60px"}
+        contentStyle={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: isMobile ? "column-reverse" : "row",
+          gap: isMobile ? "24px" : undefined,
+        }}
+        bgColor={`${tokens.colors.brand.lightCream}`}
+      >
+        <C.BoxImage>
+          <Image src={img5} alt="regularizacao" priority />
+        </C.BoxImage>
+        <C.BoxText>
+          <Anchor id="civil-causes" />
+          <Title
+            variant="secondary"
+            style={{ textAlign: "left", color: "#000" }}
+          >
+            Por que contratar uma consultoria Jurídica?
+          </Title>
+          <Subtitle style={{ color: "#000", textAlign: "left" }}>
+            Em casos cíveis, é de extrema importância buscar o aconselhamento
+            jurídico especializado para alcançar uma resolução justa e
+            eficiente. Aqui na CBL Consultoria, compreendemos a complexidade do
+            sistema legal e o impacto significativo que uma representação
+            adqueada pode ter em seu caso. Veja por que é fundamental contar com
+            um advogado especializado
+            <div style={{ marginTop: "20px" }}>
+              {civelList.map((item) => (
+                <C.CivelListInfoItem key={item.id}>
+                  <ConfirmIcon />
+                  <Subtitle style={{ color: "#000" }}>{item.title}</Subtitle>
+                </C.CivelListInfoItem>
+              ))}
+            </div>
+          </Subtitle>
+        </C.BoxText>
+      </ContentBox>
       <PurposeForm />
-      {/* </C.Bg> */}
     </>
-  );
-};
-export default LegalAdvice;
+  )
+}
+export default LegalAdvice
--- src/styles/legalAdvice.ts
+++ src/styles/legalAdvice.ts
@@ -1,51 +1,11 @@
-import styled from "styled-components";
-import { tokens } from "@/utils/tokens";
+import styled from "styled-components"
+import { tokens } from "@/utils/tokens"
 
 interface IProps {
-  bgColor?: string;
-  textColor?: string;
+  bgColor?: string
+  textColor?: string
 }
 
-export const ContainerFluid = styled.div<IProps>`
-  width: 100%;
-  margin: 0 auto;
-  position: relative;
-  background-color: ${(props) => props.bgColor};
-
-  @media (max-width: ${tokens.breakpoints.tablet}) {
-    padding: 0 24px;
-  }
-`;
-export const Container = styled.div`
-  width: 100%;
-  max-width: ${tokens.breakpoints.desktop};
-  margin: 0 auto;
-  position: relative;
-  padding: 95px 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-gap: 24px;
-
-
-  @media (max-width: ${tokens.breakpoints.tablet}) {
-    flex-direction: column;
-    padding: 20px 24px;
-  }
-`;
-export const Bg = styled.div`
-  width: 600px;
-  margin: 0 auto;
-  padding-top: 100px;
-  padding-bottom: 100px;
-
-  @media (max-width: ${tokens.breakpoints.tablet}) {
-    width: 100%;
-    padding-inline: 20px;
-    padding-top: 50px;
-    padding-bottom: 50px;
-  }
-`;
 export const BoxText = styled.div`
   display: flex;
   max-width: 500px;
@@ -56,7 +16,7 @@ export const BoxText = styled.div`
     max-width: 100%;
     gap: 20px;
   }
-`;
+`
 
 export const CivelListInfoItem = styled.div`
   display: flex;
@@ -64,32 +24,13 @@ export const CivelListInfoItem = styled.div`
   align-items: center;
   gap: 10px;
   cursor: context-menu;
-`;
+`
 
-export const Title = styled.h1<IProps>`
-  font-size: ${tokens.font.sizes.xl};
-  color: ${(props) => props.textColor};
-  max-width: 400px;
-  line-height: ${tokens.font.sizes.xxl};
-  font-weight: 600;
-  text-transform: uppercase;
-`;
-export const Text = styled.p<IProps>`
-  font-size: ${tokens.font.sizes.sm};
-  color: ${(props) => props.textColor};
-  line-height: ${tokens.font.sizes.md};
-  max-width: 100%;
-
-  @media (max-width: ${tokens.breakpoints.tablet}) {
-    font-size: ${tokens.font.sizes.xs};
-  }
-`;
 export const BoxImage = styled.div`
   width: 500px;
   height: 100%;
 
   @media (max-width: ${tokens.breakpoints.tablet}) {
     width: 100%;
-    margin-top: 20px;
   }
-`;
+`
