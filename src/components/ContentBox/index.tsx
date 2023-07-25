aa398b1a65b6ac007261b6a88ca02ffd5cc2234a
João Vitral <joaoviniciusvitral@hotmail.com>
2023-07-25 00:20:57 -0300

    fix: space

--- src/components/ContentBox/index.tsx
+++ src/components/ContentBox/index.tsx
@@ -12,7 +12,7 @@ interface ContentBoxProps extends HtmlHTMLAttributes<HTMLDivElement>, S.IContain
 const ContentBox: React.FC<ContentBoxProps> = ({ children, bgColor, style, contentStyle }) => {
   const { isMobile } = useDevice()
   return (
-    <S.Container bgColor={bgColor} py={isMobile ? tokens.space.sizeXs : tokens.space.sizeXl} style={style}>
+    <S.Container bgColor={bgColor} py={isMobile ? tokens.space.sizeXxs : tokens.space.sizeXl} style={style}>
       <S.Content style={contentStyle}>{children}</S.Content>
     </S.Container>
   )
