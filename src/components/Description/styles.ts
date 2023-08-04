d2ba7e3ae219595ece73a9df1a5ab7b7367f4c6d
João Vitral <joaoviniciusvitral@hotmail.com>
2023-08-04 16:06:03 -0300

    fix

--- src/components/Description/styles.ts
+++ src/components/Description/styles.ts
@@ -1,5 +1,5 @@
-import { tokens } from "@/utils/tokens";
-import styled from "styled-components";
+import { tokens } from "@/utils/tokens"
+import styled from "styled-components"
 
 export const Description = styled.p`
   font-family: ${tokens.font.family.secondary};
@@ -7,4 +7,15 @@ export const Description = styled.p`
   font-weight: ${tokens.font.weight.regular};
   line-height: 1.5em;
   color: ${tokens.colors.highlight.dark};
+
+  b,
+  strong {
+    font-weight: ${tokens.font.weight.bold};
+    font-family: ${tokens.font.family.secondary};
+  }
+
+  span {
+    font-family: ${tokens.font.family.secondary};
+    font-size: ${tokens.font.sizes.xxs};
+  }
 `
