ffa983549d95d6b146448a35c4ac02ab9bc9e0cf
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2023-08-15 12:38:32 -0300

    fix: margintop

--- src/sections/WhyToChoose/styles.ts
+++ src/sections/WhyToChoose/styles.ts
@@ -6,7 +6,7 @@ export const WhyList = styled.ul`
   grid-template-columns: repeat(2, 1fr);
   grid-gap: 20px;
 
-  margin-top: 70px;
+  margin-top: 32px;
 
   @media (max-width: 920px) {
     grid-gap: 10px;
